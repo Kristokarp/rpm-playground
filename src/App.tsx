d9ac4051330ea1c163ec0197db0e043b00acbcf0
Kristo Karp <kristokarp@gmail.com>
2024-09-19 12:48:57 +0300

    use nova good

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,10 @@
 function App() {
-  return <rpm-editor></rpm-editor>;
+  return (
+    <rpm-editor
+      apidomain="https://api.readyplayer.dev"
+      subdomain="next-gen-api"
+    ></rpm-editor>
+  );
 }
 
 export default App;
